7676eba13b1b1558023353252792f894a47e1cc3
Dan Casey <dcasey720>
2020-02-28 10:37:53 +0000

    Initial commit

---
+++ grunt/tasks/build.js
@@ -0,0 +1,21 @@
+/**
+ * For production
+ */
+module.exports = function(grunt) {
+  grunt.registerTask('build', 'Creates a production-ready build of the course', [
+    '_log-vars',
+    'check-json',
+    'clean:output',
+    'build-config',
+    'copy',
+    'schema-defaults',
+    'handlebars',
+    'tracking-insert',
+    'javascript:compile',
+    'clean:dist',
+    'less:compile',
+    'replace',
+    'scripts:adaptpostbuild',
+    'minify'
+  ]);
+};
---
+++ grunt/tasks/diff.js
@@ -0,0 +1,18 @@
+/**
+ * For development
+ */
+module.exports = function(grunt) {
+  grunt.registerTask('diff', 'Differential compile on a developer-friendly build of the course', [
+    '_log-vars',
+    'check-json',
+    'build-config',
+    'copy',
+    'schema-defaults',
+    'newer:handlebars:compile',
+    'tracking-insert',
+    'newer:javascript:dev',
+    'newer:less:dev',
+    'replace',
+    'scripts:adaptpostbuild'
+  ]);
+};
